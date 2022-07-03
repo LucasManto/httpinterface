e8ccc41e09ddd618efb90b37ec7622966fc2aaf7
Lucas Mantovani <lucashmantovani@hotmail.com>
2022-07-03 17:50:01 -0300

    Add client calls

--- src/main/java/com/example/httpinterface/HttpinterfaceApplication.java
+++ src/main/java/com/example/httpinterface/HttpinterfaceApplication.java
@@ -1,7 +1,12 @@
 package com.example.httpinterface;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.httpinterface.clients.TodoClient;
+import com.example.httpinterface.dtos.Todo;
 
 @SpringBootApplication
 public class HttpinterfaceApplication {
@@ -10,4 +15,12 @@ public class HttpinterfaceApplication {
 		SpringApplication.run(HttpinterfaceApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(TodoClient todoClient) {
+		return args -> {
+			System.out.println(todoClient.get(1L));
+			System.out.println(todoClient.todos());
+			System.out.println(todoClient.create(new Todo(null, "My todo", false, 1L)));
+		};
+	}
 }
